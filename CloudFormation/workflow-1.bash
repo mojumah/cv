a2c33a41c88bb5ee2c805120cc4164475c434715
Mohammad Jumah <mohammad_jumah@outlook.com>
2024-03-20 23:46:16 +0100

    vpc for jenkins ec2 instance

--- CloudFormation/workflow-1.bash
+++ CloudFormation/workflow-1.bash
@@ -1,1 +1,2 @@
+aws cloudformation create-stack --stack-name mycloudjenkins --template-body file://jenkins-vpc.yaml
 aws cloudformation create-stack --stack-name mycloudjenkins --template-body file://jenkins.yaml

b53deb557276a3244bff146eb139cd271b3ef2e9
Mohammad Jumah <mohammad_jumah@outlook.com>
2024-03-28 02:58:55 +0100

    Fixing ImportValue CloudFormation function

--- CloudFormation/workflow-1.bash
+++ CloudFormation/workflow-1.bash
@@ -1,2 +1,2 @@
 aws cloudformation create-stack --stack-name jenkins-vpc --template-body file://jenkins-vpc.yaml --parameters ParameterKey=IP,ParameterValue=$(curl -s http://checkip.amazonaws.com/)/32
-aws cloudformation create-stack --stack-name mycloudjenkins --template-body file://jenkins.yaml
+aws cloudformation create-stack --stack-name jenkins --template-body file://jenkins.yaml --parameters ParameterKey=myKeyPair,ParameterValue=jenkins

862234fc76d2cf6c69f96ba3981291d74dff91d0
Mohammad Jumah <mohammad_jumah@outlook.com>
2024-09-10 22:44:35 +0100

    Using CloudFormation to fetch the latest AMI ID from Parameter store instead of hardcoding the AMI in the syntax

--- CloudFormation/workflow-wordpress.bash
+++ CloudFormation/workflow-wordpress.bash
@@ -1,3 +1,4 @@
-aws ec2 describe-images --owners amazon --filters "Name=description,Values=Amazon Linux 2023*" --query 'sort_by(Images, &CreationDate)[].Name'
+aws ssm get-parameters --names " /aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-x86_64" --region us-east-1
+aws ec2 describe-images --owners amazon --filters "Name=description,Values=Amazon Linux 2023*" "Name=creation-date,Values=2024*"
 aws cloudformation create-stack --stack-name wordpress-vpc --template-body file://wordpress-vpc.yaml --parameters ParameterKey=IP,ParameterValue=$(curl -s http://checkip.amazonaws.com/)/32
 aws cloudformation create-stack --stack-name wordpress --template-body file://wordpress.yaml --parameters ParameterKey=myKeyPair,ParameterValue=jenkins

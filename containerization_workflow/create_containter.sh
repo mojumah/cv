dcdb26d01de044c155e4f0b3d77d8c63637d88e4
Mohammad <mohammad.jumah.uk@gmail.com>
2025-01-14 22:11:19 +0000

    using ECR instead of Docker Hub to automate the procedure

--- containerization_workflow/create_containter.sh
+++ containerization_workflow/create_containter.sh
@@ -1,3 +1,8 @@
+# If you are creating the container store manually using Docker Hub skip to step 3
+# Create a container a container store on AWS. ECR
+aws ecr create-repository \
+    --repository-name project-a/sample-repo \
+    --image-tag-mutability IMMUTABLE
 # After writing a Docker file, build the image
 sudo docker build -t jumahmohammad/cv .
 # To share your image, you have to push the image to a container repository e.g. docker hub

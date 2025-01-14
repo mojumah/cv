b66829eb8175fa384b0028804ce46a7aabd3a5dc
Mohammad Jumah <mohammad.jumah.uk@gmail.com>
2025-01-14 13:04:56 +0000

    Update create_containter.sh

--- containerization_workflow/create_containter.sh
+++ containerization_workflow/create_containter.sh
@@ -1,6 +1,6 @@
 # After writing a Docker file, build the image
 sudo docker build -t jumahmohammad/cv .
 # To share your image, you have to push the image to a container repository e.g. docker hub
-sudo dockerpush jumahmohammad/cv
+sudo docker push jumahmohammad/cv
 # To run the container
 sudo docker run -it --rm -d -p 8080:80 --name blog  jumahmohammad/cv
